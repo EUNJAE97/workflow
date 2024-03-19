97f2a0801702e9ed734793c42029f13c0b328d94
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-03-19 14:51:38 +0900

    #3 hello world 출력 기능 완성

--- src/main/java/com/ohgiraffers/chap99worflow/Chap99WorflowApplication.java
+++ src/main/java/com/ohgiraffers/chap99worflow/Chap99WorflowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Chap99WorflowApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world!");
         SpringApplication.run(Chap99WorflowApplication.class, args);
     }
 
