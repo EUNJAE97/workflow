616bc831263833c72658d16ab3a5da5b5f3cc1c8
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-03-19 12:44:25 +0900

    gd

---
+++ src/main/java/com/ohgiraffers/chap99worflow/Chap99WorflowApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.chap99worflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap99WorflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap99WorflowApplication.class, args);
+    }
+
+}
